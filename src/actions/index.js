4761352e2dec4182c923666cd0f22a5537ec8040
nicozapata220 <nicolas.a.zapata@gmail.com>
2018-01-23 20:50:25 -0300

    lesson 143

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,12 +4,21 @@ const ROOT_URL = "http://reduxblog.herokuapp.com/api"
 const API_KEY = '?key=nicozapata'
 
 export const FETCH_POSTS = "FETCH_POSTS"
+export const CREATE_POSTS = "CREATE_POSTS"
 
 export function fetchPosts() {
     const request = axios.get(`${ROOT_URL}/posts${API_KEY}`)
-    console.log(request)
     return {
         type: FETCH_POSTS,
+        payload: request
+    }
+}
+
+export function createPost(values,callback) {
+    const request = axios.post(`${ROOT_URL}/posts${API_KEY}`,values)
+        .then(()=>callback())
+    return {
+        type: CREATE_POSTS,
         payload: request
     }
 }
